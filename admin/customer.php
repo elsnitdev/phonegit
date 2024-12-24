f7ddf9a5f921f06d8bde05fe7bad0139df43ea60
elsnitdv <tinsle0609@gmail.com>
2024-12-25 05:11:26 +0700

    final

--- admin/customer.php
+++ admin/customer.php
@@ -18,6 +18,7 @@ $users = $statement->fetchAll(PDO::FETCH_ASSOC);
     <link rel="stylesheet" href="show.css">
 </head>
 <body>
+ 
 <div class="admin-container">
      
         <?php include "nav.php"; ?>
@@ -49,5 +50,52 @@ $users = $statement->fetchAll(PDO::FETCH_ASSOC);
             </tr>
         <?php endforeach; ?>
     </table>
+   
+    <div>
+    <h3>Tim kiem nguoi dung </h3>
+   
+    <form action="" method="post">
+        <input type="number" placeholder="ID" name="userID">
+        <input type="submit">
+    </form> <?php 
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['userID'])) {
+        $userID = $_POST['userID'];
+    
+        $sql = "SELECT * FROM Users WHERE userID = :userID";
+        $statement = $conn->prepare($sql);
+        $statement->bindParam(':userID', $userID, PDO::PARAM_INT);
+        $statement->execute();
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
+    }
+    if ($user&& isset($_POST['userID'])): ?>
+ 
+    <table class="user-table">
+        <tr>
+            <th>UserID</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Fullname</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Sex</th>
+            <th>Actions</th>
+        </tr>
+        <tr>
+            <td><?php echo ($user['UserID']); ?></td>
+            <td><?php echo ($user['Username']); ?></td>
+            <td><?php echo ($user['Email']); ?></td>
+            <td><?php echo ($user['Fullname']); ?></td>
+            <td><?php echo ($user['Address']); ?></td>
+            <td><?php echo ($user['Phone']); ?></td>
+            <td><?php echo ($user['Sex']); ?></td>
+            <td>
+                <a href="update.php?UserID=<?php echo ($user['UserID']); ?>">Edit</a>
+                <a href="delete.php?UserID=<?php echo ($user['UserID']); ?>">Delete</a>
+            </td>
+        </tr>
+    </table>
+<?php elseif ($_SERVER['REQUEST_METHOD'] == 'POST'): ?>
+    <p>Không tìm thấy người dùng với ID đã cho.</p>
+<?php endif; ?></div>
 </body>
 </html>
